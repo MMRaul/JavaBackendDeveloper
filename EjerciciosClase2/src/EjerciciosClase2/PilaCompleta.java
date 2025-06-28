f60742d737697bb6c0ad5d1f054b1ffe785d33bd
MMRaul <raulamayorga@gmail.com>
2025-06-28 11:47:28 -0600

    Se modifico la clase PilaCompleta.java

--- EjerciciosClase2/src/EjerciciosClase2/PilaCompleta.java
+++ EjerciciosClase2/src/EjerciciosClase2/PilaCompleta.java
@@ -1,4 +1,24 @@
 package EjerciciosClase2;
 
-public class PilaCompleta {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PilaCompleta<E> extends Stack<E> {
+
+    public void pushAll(List<? extends E> list){
+        for(E e:list){
+            super.push(e);
+        }
+
+    }
+
+    public List<? super E> popAll(){
+        List<E> outPut = new ArrayList<>();
+        while (this.empty()){
+            E e = super.pop();
+            outPut.add(e);
+        }
+        return outPut;
+    }
 }
