25207febd18787be005c45e55511195ed2bc621f
MMRaul <raulamayorga@gmail.com>
2025-07-23 12:10:51 -0600

    Ejercicios Clase 2 -Pila Completa -No Null -AddOddElements

--- EjerciciosClase2/src/EjerciciosClase2/ListaUtilitaria.java
+++ EjerciciosClase2/src/EjerciciosClase2/ListaUtilitaria.java
@@ -1,4 +1,12 @@
 package EjerciciosClase2;
 
+import java.util.List;
+
 public class ListaUtilitaria {
+
+    public static <T> void addOddElements(List<T> fuente, List<T> destino){
+        for(int i = 0; i < fuente.size(); i += 2){
+            destino.add(fuente.get(i));
+        }
+    }
 }
--- EjerciciosClase2/src/main/Main.java
+++ EjerciciosClase2/src/main/Main.java
@@ -1,22 +1,16 @@
 package main;
 
 
+import EjerciciosClase2.ListaUtilitaria;
 import EjerciciosClase2.NoNullMap;
 import EjerciciosClase2.PilaCompleta;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        HashMap<Integer,String> mapDato = new NoNullMap<>();
-
-        mapDato.put(1,"Esto no es null");
-        //mapDato.put(2,null);
-        System.out.println(mapDato.get(1));
-        //System.out.println(mapDato.get(2));
-
 
         PilaCompleta<String> listO = new PilaCompleta<String>();
         ArrayList<String> listDatos = new ArrayList<>();
@@ -27,8 +21,36 @@ public class Main {
         listDatos.add("Dato 5");
 
         listO.pushAll(listDatos);
+        System.out.println("*EJERCICIO 1 CLASE 2 - PILACOMPETA");
+        System.out.println(listO.popAll());
+        System.out.println("");
 
-        System.out.println(listO);
+
+        HashMap<Integer,String> mapDato = new NoNullMap<>();
+
+        mapDato.put(1,"Esto no es null");
+        //mapDato.put(2,null);
+
+        System.out.println("*EJERCICIO 2 CLASE 2 - NO NULL");
+        System.out.println(mapDato.get(1));
+        System.out.println(" ");
+
+
+        List<String> fuente = new ArrayList<>();
+        fuente.add("Primero");
+        fuente.add("Segundo");
+        fuente.add("Tercero");
+        fuente.add("Cuarto");
+        fuente.add("Quinto");
+
+        List<String> destino = new ArrayList<>();
+
+        ListaUtilitaria.addOddElements(fuente, destino);
+
+        System.out.println("*EJERCICIO 3 CLASE 2 - ADDODDELEMENST");
+        for(String e:destino){
+            System.out.println(e);
+        }
 
     }
 }
