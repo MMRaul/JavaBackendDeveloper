d8f59846eb75a477e49f3d12e16c2e8f314a67d9
MMRaul <raulamayorga@gmail.com>
2025-07-23 11:37:58 -0600

    Ejercicio Clase 11 Se agrego la comunicacion hacia las api´s de producto

---
+++ Proyect/orders/src/main/java/com/clase11/orders/dto/ProductResponseDto.java
@@ -0,0 +1,14 @@
+package com.clase11.orders.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductResponseDto {
+
+    private String id;
+
+}
---
+++ Proyect/orders/src/main/java/com/clase11/orders/dto/UpdateProductDto.java
@@ -0,0 +1,13 @@
+package com.clase11.orders.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateProductDto {
+
+    private String id;
+    private Integer quantity;
+
+}
